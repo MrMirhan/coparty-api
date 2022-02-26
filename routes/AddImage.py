ebe4b77ded362de2af462b6a3e71f0af3ba4621b
Mirhan <hamzamirhan.almendi@std.yeditepe.edu.tr>
2022-02-26 12:52:41 +0300

    dev

---
+++ routes/AddImage.py
@@ -0,0 +1,10 @@
+from flask import request
+import sys, time, datetime
+sys.path.append('../')
+from Functions import *
+
+def main():
+    ts = time.time()
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    if request.method == "POST":
+        return add_interest(request.form['name'], request.form['continent_name'], int(request.form['type']), request.form['description'], timestamp)
