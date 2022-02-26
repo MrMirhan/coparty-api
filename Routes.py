39ad3977eb473db7b53a44f7098eda6fb817be9a
Mirhan <hamzamirhan.almendi@std.yeditepe.edu.tr>
2022-02-26 12:51:33 +0300

    Added newly routes to route list.

--- Routes.py
+++ Routes.py
@@ -1,8 +1,20 @@
 from routes import Index
+
 from routes import Register
 from routes import Login
 from routes import Validate
+
 from routes import ProfileCreate
+
+from routes import AddEducation
+from routes import AddEducationType
+from routes import AddInterest
+from routes import AddInterestType
+from routes import AddImage
+from routes import AddImageType
+from routes import AddExperience
+from routes import AddPackage
+
 
 routes = [
     {
@@ -33,6 +45,48 @@ routes = [
         "route": "/profile/create",
         "name": "profile/create",
         "function": ProfileCreate.main,
+        "methods": ["POST"]
+    },
+    {
+        "route": "/admin/add/type/education",
+        "name": "admin/add/type/education",
+        "function": AddEducationType.main,
+        "methods": ["POST"]
+    },
+    {
+        "route": "/admin/add/type/interest",
+        "name": "admin/add/type/interest",
+        "function": AddInterestType.main,
+        "methods": ["POST"]
+    },
+    {
+        "route": "/admin/add/type/image",
+        "name": "admin/add/type/image",
+        "function": AddImageType.main,
+        "methods": ["POST"]
+    },
+    {
+        "route": "/admin/add/education",
+        "name": "admin/add/education",
+        "function": AddEducation.main,
+        "methods": ["POST"]
+    },
+    {
+        "route": "/admin/add/interest",
+        "name": "admin/add/interest",
+        "function": AddInterest.main,
+        "methods": ["POST"]
+    },
+    {
+        "route": "/admin/add/image",
+        "name": "admin/add/image",
+        "function": AddImage.main,
+        "methods": ["POST"]
+    },
+    {
+        "route": "/admin/add/package",
+        "name": "admin/add/package",
+        "function": AddPackage.main,
         "methods": ["POST"]
     }
 ]
