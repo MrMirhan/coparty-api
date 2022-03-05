f819c7a1cec63532f6f2bedaac4da73d6bbb0261
Mirhan <hamzamirhan.almendi@std.yeditepe.edu.tr>
2022-03-05 22:28:05 +0300

    dev

--- routes/Auth.py
+++ routes/Auth.py
@@ -10,11 +10,11 @@ def main():
         if not session.get("Authcode"):
             if config.API_AUTH_CODE == request.form['authcode']:
                 session['Authcode'] = request.form['authcode']
-                response = return_messages[30]
-                response['json'] = {"code": config.API_AUTH_CODE, "sent": request.form['authcode']}
-                return 
+                return return_messages[30]
             else:
-                return return_messages[31]
+                response = return_messages[31]
+                response['json'] = {"code": config.API_AUTH_CODE, "sent": request.form['authcode']}
+                return response
         else:
             return return_messages[33]
     else:
